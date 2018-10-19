751ea75656952e5b68637f7c34b0c0e5358cccbc
Dan Zaratsian <dtz001@gmail.com>
2018-10-19 12:51:34 +0000

    updated app

--- sklearn_deployment/app_nfl.py
+++ sklearn_deployment/app_nfl.py
@@ -1,7 +1,4 @@
 
-'''
-/spark/bin/spark-submit app_nfl.py 
-'''
 
 import os,sys,re, csv
 import random
@@ -53,7 +50,7 @@ def import_csv(filepath_with_name):
             row_count = row_count + 1
         
         col_count = len(row)
-    except: 
+    except:
         rows = 'Error in data location or format'
         header    = ''
         col_count = ''
@@ -62,90 +59,13 @@ def import_csv(filepath_with_name):
     return header, rows
 
 
-header, rawdata = import_csv('/assets/static/assets/nfldata2.csv')
+header, rawdata = import_csv('/static/assets/nfldata2.csv')
 
 
 def get_next_play(rawdata, row_number):
     record = rawdata[row_number]
     date   = record[0][0:10]
     return record, date
-
-
-def start_connect_to_livy_session(host='', port='8999'):
-    base_url    = 'http://' + str(host) +':'+ str(port)
-    session_url = base_url + '/sessions'
-    headers     = {'Content-Type': 'application/json', 'X-Requested-By': 'spark'}
-    try:
-        response   = requests.get(session_url).json()
-        session_id = response['sessions'][0]['id']
-    except:
-        session_id = ''
-    return session_id
-
-
-def spark_livy_interactive(host='', port='8999', code_payload='', session_id=''):
-    
-    result      = ''
-    base_url    = 'http://' + str(host) +':'+ str(port)
-    session_url = base_url + '/sessions'
-    headers     = {'Content-Type': 'application/json', 'X-Requested-By': 'spark'}
-    
-    if session_id == '':
-        #data = {'kind': 'pyspark'}
-        data = {'kind': 'pyspark', 'heartbeatTimeoutInSecond':600, 'conf':{'spark.yarn.appMasterEnv.PYSPARK_PYTHON':'/opt/anaconda2/bin/python2.7'}}
-        spark_session = requests.post(base_url + '/sessions', data=json.dumps(data), headers=headers)
-        if spark_session.status_code == 201:
-            session_id  = spark_session.json()['id']
-            session_url = base_url + spark_session.headers['location']
-            print('[ INFO ] Status Code:           ' + str(spark_session.status_code))
-            print('[ INFO ] Session State:         ' + str(spark_session.json()['state']))
-            print('[ INFO ] Session ID:            ' + str(session_id))
-            print('[ INFO ] Payload:               ' + str(spark_session.content))
-            
-            # Loop until Spark Session is ready (i.e. In the Idle State)
-            session_state = ''
-            while (session_state == '') or (session_state == 'starting'):
-                time.sleep(0.25)
-                print('[ INFO ] Session State:         ' + str(session_state))
-                session = requests.get(session_url, headers=headers)
-                if session.status_code == 200:
-                    session_state = session.json()['state']
-                else:
-                    print('[ ERROR ] Status Code: ' + str(session.status_code))
-                    session_state = 'end'
-            
-            print('[ INFO ] Session State:         ' + str(session_state))
-            print('[ INFO ] Spark App ID:          ' + str(session.json()['appId']))
-            print('[ INFO ] Spark App URL:         ' + str(session.json()['appInfo']['sparkUiUrl']))
-        else:
-            print('[ ERROR ] Failed to start Spark Session, Status Code: ' + str(spark_session.status_code))
-    else:
-        print('[ INFO ] Using existing Session ID: ' + str(session_id))
-    
-    try:
-        submit_code = requests.post(base_url + '/sessions/' + str(session_id) + '/statements', data=json.dumps(code_payload), headers=headers)
-    except:
-        result = {'state':'Error with code submission'}
-        return session_id, result
-    
-    if submit_code.status_code == 201:
-        submit_code_state = ''
-        while (submit_code_state == '') or (submit_code_state == 'running'):
-            time.sleep(0.25)
-            code_response = requests.get(base_url + submit_code.headers['location'], headers=headers)
-            if code_response.status_code == 200:
-                result = code_response.json()
-                submit_code_state = code_response.json()['state']
-                print('[ INFO ] Code Submit State:     ' + str(submit_code_state))
-                #print('\n' + '#'*50)
-                #print('[ INFO ] Result:   ' + str(result))
-                #print('#'*50)
-            else:
-                print('[ ERROR ] Status Code:   ' + str(code_response.status_code))
-    else:
-        print('[ ERROR ] Failed to submit Spark code successfully, Status Code: ' + str(submit_code.status_code))
-     
-    return session_id, result
 
 
 def predict_play(model_pass, model_run, qtr, down, TimeSecs, yrdline100, ydstogo, ydsnet, month_day, posteam, DefensiveTeam, PlayType_lag):
@@ -164,7 +84,7 @@ def predict_play_livy(model_pass, model_run, qtr, down, TimeSecs, yrdline100, yd
     
     code_payload = {'code': '''
     
-    import datetime, time 
+    import datetime, time
     import re, random, sys
     from pyspark.ml import PipelineModel
     
@@ -311,7 +231,7 @@ def api():
         #PlayType_lag    = 'Run'
         
         best_play, passing_yards, running_yards = predict_play(model_pass, model_run, qtr, down, TimeSecs, yrdline100, ydstogo, ydsnet, month_day, posteam, DefensiveTeam, PlayType_lag)
-        #('Running Play', 3.984419701538829, 5.375515688399204)                          
+        #('Running Play', 3.984419701538829, 5.375515688399204)
         
         return jsonify( {"best_play":best_play, "passing_yards":passing_yards, "running_yards":running_yards } )
 
@@ -325,22 +245,8 @@ def api():
 
 if __name__ == "__main__":
     
-    from pyspark.sql import SparkSession
-    from pyspark.ml import PipelineModel
-    
-    spark = SparkSession \
-        .builder \
-        .config("spark.driver.allowMultipleContexts", "true") \
-        .appName("pyspark_nfl_app") \
-        .getOrCreate()
-    
-    model_pass = PipelineModel.load('/assets/static/assets/nfl_model_pass')
-    model_run  = PipelineModel.load('/assets/static/assets/nfl_model_run')    
-    #model_pass = PipelineModel.load('./static/assets/nfl_model_pass')
-    #model_run  = PipelineModel.load('./static/assets/nfl_model_run')
-    
     #app.run(debug=True, threaded=False, host='0.0.0.0', port=4444)
-    app.run(threaded=False, host='0.0.0.0', port=4444)
+    app.run(threaded=False, host='0.0.0.0', port=8500)
 
 
 
